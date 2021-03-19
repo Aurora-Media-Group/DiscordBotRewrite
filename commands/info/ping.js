cd3bb28ed185e144d5d6528b8e721db15ef28d63
BHopperJanix <73129856+BHopperJanix@users.noreply.github.com>
2021-03-19 14:43:36 +0200

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -3,7 +3,8 @@ module.exports = {
   run: async (client, message, args) => {
     message.channel.send(`Finding bot's ping`).then(msg => {
       const ping = msg.createdTimestamp - message.createdTimestamp;
-      msg.edit(`Bot's ping is ${ping}ms`)
+      msg.edit(`Bot's ping is ${ping}ms`);
+      message.channel.send(`pong`)
     })
   }  
 }
