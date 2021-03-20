1d6cf3189b8960a43df4d477d428c7922a8e5c21
Dan Howe <danhowe0@gmail.com>
2021-03-20 10:36:54 +0000

    Fixed ping and help command

--- commands/info/help.js
+++ commands/info/help.js
@@ -15,51 +15,38 @@ module.exports = {
   aliases: command_data.Aliases,  // You do not need to touch these
 
   run: async (client, message, args) => {
-	// -- Code --
-
-	const embed = new MessageEmbed();
-
-	var found = true;
-
-	if (args[0]) {
-    if(help_data[args[0]]) {
-		embed.setTitle(args[0]);
-
-		for (command in help_data[args[0]]) {
-      console.log(command)
-      console.log(command["Description"])
-			
-			embed.addField({
-        name: command, 
-        value: help_data[args[0]][command]["Description"]
-      });
-		}
-
-	  } else {
-		  for (category in help_data) {
-          if (category[args[0]]) {
-            embed.setTitle(args[0]);
-            embed.add_field({name: "Aliases", value: category.args[0].Aliases});
-            embed.add_field({name: "Description", value: category.args[0].Description});
-            embed.add_field({name: "Usage", value: category.args[0].Usage});
-            message.channel.send(embed);
-            return; 
-          }
-        }
-      }
-    } else {
-			embed.setTitle("Help Menu");
-
+    
+    const embed = new MessageEmbed();
+    if (!args[0]) {
+      embed.setTitle("Help Menu");
       for (category in help_data) {
         command_string = ""
         for (command in help_data[category]) {
           command_string += "`"+ command +"`, "
+          
         }
         embed.addField(category, command_string)
       }
-				
-		}
-  message.channel.send(embed)
-  }
-  
+
+    } else  {
+      for (ctg in help_data) {
+        if (args[0] === ctg) {
+          embed.setTitle(ctg)
+          for (command in help_data[ctg]) {
+            var cmd = command
+            var desc = help_data[ctg][command].Description
+
+            console.log(cmd)
+            console.log(desc)
+
+            embed.addField(cmd, desc)
+
+          }
+        }
+      }
+
+    }  
+    message.channel.send(embed)
+	}
+
 }
--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,5 +1,17 @@
+// -- Imports --
+
+const { Info } = require("../../commands.json"); 
+const command_data = Info.ping;          // change Category and command_name
+const {MessageEmbed} = require("discord.js");
+
+// ------------
+
+
+// Information
 module.exports = {
   name: "ping",
+  aliases: command_data.Aliases,
+
   run: async (client, message, args) => {
     message.channel.send(`Finding bot's ping`).then(msg => {
       const ping = msg.createdTimestamp - message.createdTimestamp;
