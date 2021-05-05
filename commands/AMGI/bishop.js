cd0e9ffdb8bdbcabccc710adce1d5c5c9d821141
Dan Howe <>
2021-05-05 20:32:03 +0000

    added a bishop command

---
+++ commands/AMGI/bishop.js
@@ -0,0 +1,20 @@
+const { AMGI } = require("../../dev_commands.json");  
+const command_data = AMGI.bishop;
+
+module.exports = {
+    name:'bishop',
+    aliases: command_data.Aliases,
+    run: async(client, message, args) => {
+      const bishop = client.emojis.cache.find(emoji => emoji.name === "Bishop");
+      message.channel.send(`${bishop}`)
+
+      random = Math.floor(Math.random() * 10); 
+
+      if (random === 0) {  
+        message.channel.send(`Bishop Good`)
+      } else {
+        message.channel.send(`Bishop Bad`)
+      }
+        
+    }
+}
