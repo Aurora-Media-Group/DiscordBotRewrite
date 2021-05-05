24a2e9b97554fbb91abc2caf481a7116e24b0cf8
Dan Howe <>
2021-05-05 21:09:25 +0000

    changed bishop probability

--- commands/AMGI/bishop.js
+++ commands/AMGI/bishop.js
@@ -1,19 +1,19 @@
-const { AMGI } = require("../../commands.json");  
+const { AMGI } = require('../../commands.json');
 const command_data = AMGI.bishop;
 
 module.exports = {
-    name:'bishop',
-    aliases: command_data.Aliases,
-    run: async(client, message, args) => {
-      const bishop = client.emojis.cache.find(emoji => emoji.name === "Bishop");
-      message.channel.send(`${bishop}`)
+	name: 'bishop',
+	aliases: command_data.Aliases,
+	run: async (client, message, args) => {
+		const bishop = client.emojis.cache.find(emoji => emoji.name === 'Bishop');
+		message.channel.send(`${bishop}`);
 
-      random = Math.floor(Math.random() * 10); 
+		random = Math.floor(Math.random() * 2);
 
-      if (random === 0) {  
-        message.channel.send(`Bishop Good`)
-      } else {
-        message.channel.send(`Bishop Bad`)
-      }
-    }
-}
+		if (random === 0) {= 
+			message.channel.send(`Bishop Good`);
+		} else {
+			message.channel.send(`Bishop Bad`);
+		}
+	}
+};
