e4533b39863e75b1dd915e4812bde59896d26472
DanHowe0 <>
2021-05-21 17:36:54 +0000

    FIXED HELP COMMAND!!!

--- commands/info/help.js
+++ commands/info/help.js
@@ -43,11 +43,10 @@ module.exports = {
       embed.setTitle(args[1])
       for (item in help_data[args[0]][args[1]]) {
         val = help_data[args[0]][args[1]][item]
-        console.log(val[0])
-        if (val[0] === undefined) {val = "None"}
+        if (val.length === 0) {val = "None"}
         embed.addField(
           item,
-          help_data[args[0]][args[1]][item]
+          val
         )
       }
     
